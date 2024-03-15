5a0ce7c61e7df224f626a34963c53aa65883dbd7
Nodirjonov <sobirovakomila85@gmail.com>
2024-03-15 18:03:48 +0500

    js10

---
+++ script.js
@@ -0,0 +1,28 @@
+// declaretion
+// azizhoja();
+// function azizhoja() {
+//     console.log("salom");
+// }
+
+// expression
+// const myfuction = function () {
+//     console.log('alik');
+// }
+// myfuction();
+
+// arrow
+// const luboy = (name) => {
+//     console.log(`salom ${name}`);
+// }
+// luboy('fayzulloh');
+
+let a = +prompt("Введите первое число")
+let b = +prompt("Введите второе число")
+function c (second) {
+    if(a>b) {
+        alert(`Самое большое число: ${a}`);
+    }else {
+        alert(`Самое большое число: ${b}`);
+    }
+}
+c();
